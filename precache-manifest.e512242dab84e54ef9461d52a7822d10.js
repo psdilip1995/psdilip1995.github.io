744e7bd4e0f8f351bf80c1e931dc290d8b5ac648
psdilip1995 <31354039+psdilip1995@users.noreply.github.com>
2020-07-07 17:52:32 -0400

    Add files via upload
    E-Portfolio

---
+++ precache-manifest.e512242dab84e54ef9461d52a7822d10.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f42fabde632addaafed9f1e330989897",
+    "url": "/index.html"
+  },
+  {
+    "revision": "94ada2292decec3033c4",
+    "url": "./static/css/2.ce9c8e0f.chunk.css"
+  },
+  {
+    "revision": "6dbdd562b2eaa9be599c",
+    "url": "./static/css/main.aef610e0.chunk.css"
+  },
+  {
+    "revision": "94ada2292decec3033c4",
+    "url": "./static/js/2.58c5bd8f.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "./static/js/2.58c5bd8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6dbdd562b2eaa9be599c",
+    "url": "./static/js/main.b1adf2a3.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "./static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "768cb3bb2eb722f5031179368f36865a",
+    "url": "./static/media/D3_Code_Builder.768cb3bb.png"
+  },
+  {
+    "revision": "ae2d553878282496e9fc658c1047ff05",
+    "url": "./static/media/MyImage.ae2d5538.jpeg"
+  },
+  {
+    "revision": "1c547dc442a29412afc458f2f4ea7d67",
+    "url": "./static/media/alexa.1c547dc4.png"
+  },
+  {
+    "revision": "fa4b48deaffe590d8c9d003162192411",
+    "url": "./static/media/bg.fa4b48de.jpg"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "./static/media/burger-logo.b8503d26.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "./static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "./static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "./static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "./static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "./static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "0d825b1d1880ad7089801edd62122431",
+    "url": "./static/media/heater.0d825b1d.gif"
+  },
+  {
+    "revision": "9257db7aaeb16278907f2c67a472ce06",
+    "url": "./static/media/ipl.9257db7a.png"
+  },
+  {
+    "revision": "2478739240085a449ff4e253da9af23e",
+    "url": "./static/media/mapreduce.24787392.png"
+  },
+  {
+    "revision": "67c6f12f60d7b2a76ab4362ce695e89c",
+    "url": "./static/media/raft.67c6f12f.png"
+  },
+  {
+    "revision": "3e9767c4338830a27ace52b549251b34",
+    "url": "./static/media/shark2.3e9767c4.png"
+  },
+  {
+    "revision": "bf3f5f0da0945ea3ca3ef2c2c27b6f97",
+    "url": "./static/media/vis2.bf3f5f0d.png"
+  }
+]);
